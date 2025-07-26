771ea59943f9c021c0ae99df3a677c9edf04764b
maboelion <mareike.boettcher@gmail.com>
2025-07-26 12:19:33 +0200

    current location added

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ function updateTime() {
 
 function updateCity(event) {
 	let cityTimezone = event.target.value;
+	if (cityTimezone === "current") {
+		cityTimezone = moment.tz.guess();
+	}
 	let cityName = cityTimezone.split("/")[1];
 	let cityTime = moment().tz(cityTimezone);
 	let citiesElement = document.querySelector("#cities");
